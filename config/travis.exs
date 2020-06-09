165afebeb59449b71558e733cc7eb3e5d5b8ca32
vtomogz <vtomogz@gmail.com>
2020-06-09 14:54:16 +0200

    test

--- config/travis.exs
+++ config/travis.exs
@@ -7,7 +7,6 @@ use Mix.Config
 # Run `mix help test` for more information.
 config :debtmanager, Debtmanager.Repo,
   username: "postgres",
-  password: "",
   database: "debtmanager_test#{System.get_env("MIX_TEST_PARTITION")}",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
