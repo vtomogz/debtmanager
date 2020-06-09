4f97121facd9247d116adcd2d8365fe7bfc00a59
vtomogz <vtomogz@gmail.com>
2020-06-09 15:45:13 +0200

    test

--- config/travis.exs
+++ config/travis.exs
@@ -15,7 +15,7 @@ config :debtmanager, Debtmanager.Repo,
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :debtmanager, DebtmanagerWeb.Endpoint,
-  http: [port: 5432],
+  http: [port: 5433],
   server: false
 
 # Print only warnings and errors during test
